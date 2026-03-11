1280e97d0e22990396f5522c2fec2b5390896025
Seth Hauben <seth.hauben@gmail.com>
2026-03-11 14:23:06 -0400

    Use implicit auth flow for Google OAuth compatibility on static site

--- supabase-config.js
+++ supabase-config.js
@@ -2,4 +2,6 @@ const SUPABASE_URL = 'https://acpzzaikuoyjyfayqxic.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjcHp6YWlrdW95anlmYXlxeGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMTI1ODcsImV4cCI6MjA4ODY4ODU4N30.HS52IlfdSJiCVt6jOhHVc1-qOpmx-1wadmurT5_hrRg';
 
 const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { flowType: 'implicit' }
+});
